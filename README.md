a7cf9da91719114da53fd3a3c5a0bb23b9a91393
Jose Sanchez <jparkers0012@gmail.com>
2014-05-14 17:37:46 +0000

    First commit and README update

---
+++ README.md
@@ -0,0 +1,2 @@
+## Bloccit: A Reddit clone to teach the fundamentals of web development using Rails 4
+Made with my mentor at Bloc[www.bloc.io]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccit::Application.config.secret_key_base = 'b16e706760eea0e544171e753c65ba1ee89f9a7f4033f6a49fbeea74921e21e30f84f7e327368eee2b05e051e4bd7dcd960cf01800de0bfbfd66ea54457dcf6b'
