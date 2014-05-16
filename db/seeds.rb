74935ca40962027d5079919c10c8e7453d67163b
Jose Sanchez <jparkers0012@gmail.com>
2014-05-16 02:25:55 +0000

    Associated users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
 require 'faker'
 
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
 50.times do
   Post.create(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
     )
@@ -16,6 +28,11 @@ posts = Post.all
     )
 end
 
+User.first.update_attributes(
+  email: 'hailex@live.com',
+  password: 'password',
+  )
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
