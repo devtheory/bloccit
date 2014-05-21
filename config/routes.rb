d73e85f321cfebf3340580bbca43871651f308c5
Jose Sanchez <jparkers0012@gmail.com>
2014-05-21 14:36:29 +0000

    Associated and nested topics and posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Bloccit::Application.routes.draw do
 
   devise_for :users
-  resources :posts
+
+  #nested routes
+  resources :topics do
+    resources :posts, except: [:index]
+  end
+  
 
   get 'about' => 'welcome#about'
   
