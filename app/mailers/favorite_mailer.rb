0152828ddcdace4bf403b157936e8ee9831947fb
Jose Sanchez <jparkers0012@gmail.com>
2014-05-26 17:16:40 +0000

    Implemented favoriting and cleaned up CSS

---
+++ app/mailers/favorite_mailer.rb
@@ -0,0 +1,15 @@
+class FavoriteMailer < ActionMailer::Base
+  default from: "hailex@live.com"
+
+  def new_comment(user, post, comment)
+    @user = user
+    @post = post
+    @comment = comment
+
+    headers["Message-ID"] = "<comments/#{@comment.id}@devtheory-bloccit.com>"
+    headers["In-Reply-To"] = "<post/#{@post.id}@devtheory-bloccit.com>"
+    headers["References"] = "<post/#{@post.id}@devtheory-bloccit.com>"
+
+    mail(to: user.email, subject: "New comment on #{@post.title}")
+  end
+end
